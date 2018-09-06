95e34747c8fa0fd466ca7b33b03f540f041b1e13
Nicolai M. Olesen <kontakt@convey.dk>
2018-09-06 15:50:37 +0200

    Composer package tests

--- field.php
+++ field.php
@@ -2,10 +2,18 @@
 use Carbon_Fields\Carbon_Fields;
 use Carbon_Field_UrlPicker\UrlPicker_Field;
 
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require( __DIR__ . '/vendor/autoload.php' );
+}
+
 define('Carbon_Field_UrlPicker\\DIR', __DIR__);
 
-add_action('after_setup_theme', function () {
-	Carbon_Fields::extend(UrlPicker_Field::class, function ($container) {
-		return new UrlPicker_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
-	});
-}, 99);
+class Carbon_UrlPicker_Field
+{
+	public static function boot()
+	{
+		Carbon_Fields::extend(UrlPicker_Field::class, function ($container) {
+			return new UrlPicker_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
+		});
+	}
+}
